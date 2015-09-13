4575c8ea29c217566c62a08d5e7d789808369539
OssamaZ <w4rz4zi@gmail.com>
2015-09-13 18:22:51 +0100

    fix indentations

--- App/index.js
+++ App/index.js
@@ -9,40 +9,40 @@ import React from 'react-native';
 import Welcome from './components/Welcome';
 
 let {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
 } = React;
 
 class Application extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
 
-                <Welcome />
+        <Welcome />
 
-                <Text style={styles.instructions}>
-                  Press Cmd+R to reload,{'\n'}
-                  Cmd+D or shake for dev menu
-                </Text>
-            </View>
-        );
-    }
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
 };
 
 let styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
 AppRegistry.registerComponent('Application', () => Application);
--- App/stores/AppStore.js
+++ App/stores/AppStore.js
@@ -12,48 +12,48 @@ import _ from 'lodash';
 let CHANGE_EVENT = 'change';
 
 let _state = {
-    message: 'Welcome to React Native'
+  message: 'Welcome to React Native'
 };
 
 
 let AppStore = _.assign({}, EventEmitter.prototype, {
 
-    getMessage: function() {
-        return _state.message;
-    },
+  getMessage: function() {
+    return _state.message;
+  },
 
-    emitChange: function() {
-        this.emit(CHANGE_EVENT);
-    },
-    
-    addChangeListener: function(callback) {
-        this.on(CHANGE_EVENT, callback);
-    },
-    
-    removeChangeListener: function(callback) {
-        this.removeListener(CHANGE_EVENT, callback);
-    }
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+  
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+  
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
 
 });
 
 // Register callback to handle all updates
 AppDispatcher.register(function(action) {
 
-    switch(action.actionType) {
+  switch(action.actionType) {
 
-        case AppConstants.GET_MESSAGE:
-            _getStateMessage(action.user);
-            AppStore.emitChange();
-        break;
+    case AppConstants.GET_MESSAGE:
+      _getStateMessage(action.user);
+      AppStore.emitChange();
+    break;
 
-        case AppConstants.SET_MESSAGE:
-            _setStateMessage(action.message);
-            AppStore.emitChange();
-        break;
+    case AppConstants.SET_MESSAGE:
+      _setStateMessage(action.message);
+      AppStore.emitChange();
+    break;
 
-        default:
-          // no op
-    }
+    default:
+      // no op
+  }
 
 });
 
@@ -62,7 +62,7 @@ AppDispatcher.register(function(action) {
 let _getStateMessage = () => _state.message;
 
 let _setStateMessage = (message) => {
-    _state.message = message;
+  _state.message = message;
 };
 
 export default AppStore;
